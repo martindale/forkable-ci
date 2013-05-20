66793ff72f1e5105c26b8f3a561b3650b83d690c
Brian Marks <brianpmarks@gmail.com>
2013-05-20 02:38:17 -0400

    sshexec task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,23 @@
 module.exports = function(grunt) {
+  var remote = grunt.file.readJSON(grunt.option('remote'));
+  var pk = grunt.file.read(remote.privateKey);
   grunt.initConfig({
     sshexec: {
-      staging: {
-        command: ""
-      },
-      production: {
-        command: ""
-      },
-      uptime: {
-        command: "uptime",
+      deploy: {
+        command: "cd /usr/local/node/forkable-ci && grunt clone",
         options: {
-          host: "tomango.asmallorange.com",
-          username: "brianpm1",
-          privateKey: grunt.file.read("/Users/brian/.ssh/aso_id_dsa"),
-          passphrase: process.env.PASSPHRASE
+          host: remote.host,
+          username: remote.username,
+          privateKey: grunt.file.read(remote.privateKey),
+          passphrase: process.env.PASS
         }
       }
     },
-    deploy: {
-    },
     shell: {
-      ls: {
-        command: "ls -al",
-        options: {
-          stdout: true
-        }
-      },
-      deploy: {
-        command: "",
-        options: {
-        }
-      },
       clone: {
         command: [
           "git clone git@github.com:coursefork/forkshop.git",
+          "cd forkshop",
           "npm install"
         ].join('&&'),
         options: {
@@ -47,15 +31,9 @@ module.exports = function(grunt) {
     }
   });
 
-  // grunt-shell
-  // grunt-exec
-
   grunt.loadNpmTasks('grunt-ssh');
   grunt.loadNpmTasks('grunt-shell');
 
-  grunt.registerTask('uptime', ['sshexec:uptime']);
-
-  grunt.registerTask('ls', ['shell:ls']);
-
   grunt.registerTask('clone', ['shell:clone']);
+  grunt.registerTask('deploy', ['sshexec:deploy']);
 }
